5c83a8f1d2452289e75f5735a8d0a2fb726920fb
lasteChomp <163031341+lasteChomp@users.noreply.github.com>
2026-06-13 17:00:57 +0300

    feat: migrate to structured message history with user roles

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -9,23 +11,31 @@ def main():
     if api_key is None:
         raise RuntimeError("api key not found")
     
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
     client = genai.Client(api_key=api_key)
     model = "gemini-2.5-flash"
-    content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    content = args.user_prompt
 
-    generated_content = client.models.generate_content(model=model, contents=content)
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=content)])
+    ]
 
-    if generated_content.usage_metadata is None:
+    response = client.models.generate_content(model=model, contents=messages)
+
+    if response.usage_metadata is None:
         raise RuntimeError("api request failed")
 
-    tokens_prompt = generated_content.usage_metadata.prompt_token_count
-    tokens_response = generated_content.usage_metadata.candidates_token_count 
+    tokens_prompt = response.usage_metadata.prompt_token_count
+    tokens_response = response.usage_metadata.candidates_token_count 
     
     print(f"User prompt: {content}")
     print(f"Prompt tokens: {tokens_prompt}")
     print(f"Response tokens: {tokens_response}")
     print("Response:")
-    print(generated_content.text)
+    print(response.text)
 
 
 
