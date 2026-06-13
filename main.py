02f3a706ea55d849a4802dec31d85001c50763a7
lasteChomp <163031341+lasteChomp@users.noreply.github.com>
2026-06-13 16:22:24 +0300

    feat: add Gemini API client and generate content from prompt

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError("api key not found")
+    
+    client = genai.Client(api_key=api_key)
+    model = "gemini-2.5-flash"
+    content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    generated_content = client.models.generate_content(model=model, contents=content)
+    
+    print(generated_content.text)
+
 
 
 if __name__ == "__main__":
